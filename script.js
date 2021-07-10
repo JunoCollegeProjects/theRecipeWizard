6d0363003f9e876a930163eda33161656cd6d4b7
Gavyn Holt <gavynholt@gmail.com>
2021-07-10 13:47:46 -0400

    added recipe info to seach params

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const apiKey = "92cf896d674746e9b22c1a0c561637cd";
 
 const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
 url.search = new URLSearchParams({
-    query: "pasta",
-    apiKey: apiKey,
+  query: "pasta",
+  addRecipeInformation: true,
+  apiKey: apiKey,
 });
 
 fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    });
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  });
