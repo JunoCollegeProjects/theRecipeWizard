e429ffdd9064c0f35906a64087d44430a712fa3c
Sherry <sherry.t@live.ca>
2021-07-11 11:00:32 -0400

    added user input form and event listener for submit

--- script.js
+++ script.js
@@ -17,23 +17,36 @@ app.apiKey = "92cf896d674746e9b22c1a0c561637cd";
 // Init method that kicks everything off
 app.init = () => {
   console.log('Woohoo initialized!');
+  const recipeSearch = document.querySelector("#recipeSearch");
+  recipeSearch.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+    const recipe = document.querySelector("#recipe").value.trim();
+    console.log(recipe);
+  });
+}
+app.getRecipe = (userInput) => {
+  // Establish connection with API 
+  const url = new URL(app.apiUrl);
+  url.search = new URLSearchParams({
+    query: userInput,
+    addRecipeInformation: true,
+    instructionsRequired: true,
+    number: 5,
+    apiKey: app.apiKey,
+  });
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
 }
 
-// Establish connection with API 
-const url = new URL(app.apiUrl);
-url.search = new URLSearchParams({
-  query: "soup",
-  addRecipeInformation: true,
-  instructionsRequired: true,
-  number: 5,
-  apiKey: app.apiKey,
-});
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-  });
+// app.handleFormSubmit = (e) => {
+//   e.preventDefault();
+// }
 
 
 
