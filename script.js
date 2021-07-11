927df16db7cb1d0f8515a3e1fc9141fda548cb39
Sherry <sherry.t@live.ca>
2021-07-11 10:22:40 -0400

    added basic styles to scss and added namespacing

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const apiKey = "92cf896d674746e9b22c1a0c561637cd";
+// === PSEUDO CODE ===
+// User types ingredients one at a time, pressing ENTER in between. 
+// Ingredients are added to an "ingredient pool". 
+// When user presses submit, send data with API call.
+// API will return recipes as an array.
+// Loop through array and for each item, generate html for a recipe card and attach to DOM.
+// When user clicks on card, open up recipe in a modal.
+// When user presses the close button, the modal closes.
 
-const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+
+
+// Create namespaced object
+const app = {};
+app.apiUrl = "https://api.spoonacular.com/recipes/complexSearch";
+app.apiKey = "92cf896d674746e9b22c1a0c561637cd";
+
+// Init method that kicks everything off
+app.init = () => {
+  console.log('Woohoo initialized!');
+}
+
+// Establish connection with API 
+const url = new URL(app.apiUrl);
 url.search = new URLSearchParams({
-  query: "pasta",
+  query: "soup",
   addRecipeInformation: true,
-  apiKey: apiKey,
+  instructionsRequired: true,
+  number: 5,
+  apiKey: app.apiKey,
 });
 
 fetch(url)
@@ -12,3 +34,9 @@ fetch(url)
   .then(data => {
     console.log(data);
   });
+
+
+
+
+// Calling init function
+app.init();
