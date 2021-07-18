5b42eba1cdecb713ff0416fca147d27c95d5ff67
Gavyn Holt <gavynholt@gmail.com>
2021-07-18 15:23:49 -0400

    cleaned up and reorganized functions surrounding displayModal. Moved some functions inside. Also removed console logs and added comments

--- script.js
+++ script.js
@@ -11,14 +11,14 @@
 // Create namespaced object
 const app = {};
 app.apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
-// app.apiKey = "92cf896d674746e9b22c1a0c561637cd"; //gavynholt@gmail.com
-app.apiKey = "4cf2ce5f469347068595ffb2fdb1bde9"; //gholtdrums@gmail.com
+app.apiKey = "92cf896d674746e9b22c1a0c561637cd"; //gavynholt@gmail.com
+// app.apiKey = "4cf2ce5f469347068595ffb2fdb1bde9"; //gholtdrums@gmail.com
 // app.apiKey = "b3d56d5ae01547b1a1ab1a556e0974fb"; // sherryyyt@gmail.com
 
 // Recipe Card Result Array
 app.recipeObjectsArray = [];
 
-app.getRecipe = userInput => {
+app.getRecipe = (userInput) => {
   // Establish connection with API
   const url = new URL(app.apiUrl);
   url.search = new URLSearchParams({
@@ -29,16 +29,16 @@ app.getRecipe = userInput => {
   });
 
   // returns a promise
-  return fetch(url).then(res => res.json());
+  return fetch(url).then((res) => res.json());
 };
 
-app.handleSearchForm = e => {
+app.handleSearchForm = (e) => {
   e.preventDefault();
   // parse individual <li>'s into a CSV for the API
   const recipe = app.parseIngredientsToQuery();
   app
     .getRecipe(recipe)
-    .then(data => {
+    .then((data) => {
       if (data.length > 0) {
         app.recipeObjectsArray = data;
         app.displayRecipeCards(data);
@@ -46,23 +46,21 @@ app.handleSearchForm = e => {
         throw new Error("No Results Found. Try some different ingredients?");
       }
     })
-    .catch(error => {
+    .catch((error) => {
       const cardContainer = document.querySelector(".recipeResults");
       cardContainer.innerHTML = `<li>${error}</li>`;
-      console.log(error);
     });
 };
 
-app.addIngredientToContainer = e => {
+app.addIngredientToContainer = (e) => {
   e.preventDefault();
   const ingredientContainerUl = document.querySelector("#searchContainer ul");
   const inputField = e.target.querySelector("input");
   // create an array for possible multiple inputs, split by commas and trim each item of any whitespace
-  const ingredientArray = inputField.value.split(",").map(item => item.trim());
-  console.log(ingredientArray);
+  const ingredientArray = inputField.value.split(",").map((item) => item.trim());
   for (ingredient of ingredientArray) {
+    // if ingredient contains text, make an li with content
     if (ingredient != "") {
-      console.log(ingredient);
       const ingredientLiElement = document.createElement("li");
       ingredientLiElement.setAttribute("tabindex", 0);
       ingredientLiElement.innerText = ingredient;
@@ -75,7 +73,6 @@ app.addIngredientToContainer = e => {
       deleteIngredientButton.addEventListener("click", app.removeLiElement);
       // event listener for DELETE keydown press to delete li
       ingredientLiElement.addEventListener("keydown", function (e) {
-        console.log("DELETE?:", e);
         if (e.key.toLowerCase() == "delete") {
           // if ul has a next sibling, move focus forward before deleting current li (? mark prevents error for case of single li)
           e.target.nextElementSibling?.focus();
@@ -89,7 +86,7 @@ app.addIngredientToContainer = e => {
 };
 
 // Function to remove ingredient when user clicks on ingredient li element
-app.removeLiElement = e => {
+app.removeLiElement = (e) => {
   const liElement = e.target.closest("li");
   liElement.remove();
 };
@@ -100,6 +97,7 @@ app.clearIngredientList = () => {
   ul.innerHTML = "";
 };
 
+// Function to parse ingredient li's to API readable CSV (Comma Separated Values)
 app.parseIngredientsToQuery = () => {
   // Grabs search parameters in the form of an array
   const searchParams = document.querySelectorAll("#searchContainer ul li");
@@ -110,17 +108,16 @@ app.parseIngredientsToQuery = () => {
       searchQuery += ",";
     }
   }
-  console.log(searchQuery);
   return searchQuery;
 };
 
-app.displayRecipeCards = resultArray => {
+app.displayRecipeCards = (resultArray) => {
   const cardContainer = document.querySelector(".recipeResults");
   cardContainer.innerHTML = "";
 
   for (i = 0; i < resultArray.length; i++) {
     if (resultArray[i].usedIngredientCount > 0) {
-      const parseIngredientNames = ingredientName => {
+      const parseIngredientNames = (ingredientName) => {
         const ingredientsArray = [];
         for ({ name } of resultArray[i][ingredientName]) {
           ingredientsArray.push(name);
@@ -142,28 +139,27 @@ app.displayRecipeCards = resultArray => {
       `;
       recipeLiElement.setAttribute("data-id", i);
       cardContainer.appendChild(recipeLiElement);
-      console.log(resultArray[i]);
     }
   }
 
   // add event listeners to each button
   const openModalButtons = document.querySelectorAll(".openModal");
-  console.log(openModalButtons);
   for (button of openModalButtons) {
     button.addEventListener("click", app.displayModal);
   }
 };
 
-app.displayModal = async e => {
+app.displayModal = async (e) => {
   const modalRoot = document.querySelector(".modalRoot");
   // get index to use with recipeObjectsArray
   const index = e.target.closest("li").dataset.id;
   // get recipe ID for a new API call
   const recipeID = app.recipeObjectsArray[index].id;
 
+  // Make API call to retrieve detailed recipe info for Modal
   const recipeObj = await app.getRecipeInfoByID(recipeID);
-  console.log(recipeObj);
-  
+
+  //create a ul of ingredient items for display under "Ingredients" heading
   const nutritionUlElement = document.createElement("ul");
   for ({ amount, unit, name } of recipeObj.nutrition.ingredients) {
     const nutritionLiElement = document.createElement("li");
@@ -171,29 +167,30 @@ app.displayModal = async e => {
     nutritionUlElement.appendChild(nutritionLiElement);
   }
 
+  // create a ul of selected nutrients only, displayed under "Nutrients at a Glance"
   const nutrientsUlElement = document.createElement("ul");
   const nutrientsArray = ["Calories", "Protein", "Fat", "Carbohydrates"];
-    for ({amount, unit, name } of recipeObj.nutrition.nutrients) { 
-      if (nutrientsArray.includes(name)) {
-        const nutrientsLiElement = document.createElement("li");
-        nutrientsLiElement.textContent = `${amount}${unit} of ${name}`;
-        nutrientsUlElement.appendChild(nutrientsLiElement);
-      }
+  for ({ amount, unit, name } of recipeObj.nutrition.nutrients) {
+    if (nutrientsArray.includes(name)) {
+      const nutrientsLiElement = document.createElement("li");
+      nutrientsLiElement.textContent = `${amount}${unit} of ${name}`;
+      nutrientsUlElement.appendChild(nutrientsLiElement);
     }
+  }
 
   // Function to convert mins to hours
   const convertMinutes = (readyInMinutes) => {
     if (readyInMinutes >= 60) {
       const hour = Math.floor(readyInMinutes / 60);
       const minute = readyInMinutes % 60;
-      const hourString = hour > 1? "hours" : "hour";
-      const minuteString = minute == 1? "minute" : "minutes";
+      const hourString = hour > 1 ? "hours" : "hour";
+      const minuteString = minute == 1 ? "minute" : "minutes";
       return `${hour} ${hourString} and ${minute} ${minuteString}`;
     } else {
       const minuteString = readyInMinutes == 1 ? "minute" : "minutes";
       return `${readyInMinutes} ${minuteString}`;
     }
-  }
+  };
   const stringHoursMinutes = convertMinutes(recipeObj.readyInMinutes);
 
   modalRoot.innerHTML = `
@@ -216,65 +213,71 @@ app.displayModal = async e => {
     </div>
   `;
 
+  // cache some nodes after modal display
+  const currentModal = modalRoot.querySelector(".modal");
+  const closeModalButton = modalRoot.querySelector(".closeModal");
+  const printButton = modalRoot.querySelector(".printModal");
+
+  // set Modal position to current window scroll height
+  currentModal.style.top = `${Math.round(window.scrollY)}px`;
   // add class to modalRoot to display
   modalRoot.classList.add("show");
-  const currentModal = modalRoot.querySelector(".modal");
-  currentModal.style.top = `${Math.round(window.scrollY)}px`;
 
-  // event listener to handle scrolling upwards for modals
-  document.addEventListener("scroll", function() {
+  // Functions for Event Listeners
+  const updateModalPosition = () => {
     const currentScrollPosition = Math.round(window.scrollY);
     const modalScrollPosition = parseInt(currentModal.style.top, 10);
+    // if user scrolls up the page, move the modal with it
     if (currentScrollPosition < modalScrollPosition) {
       currentModal.style.top = `${currentScrollPosition}px`;
     }
-  });
+  };
 
+  const printRecipe = () => {
+    newWin = window.open("");
+    newWin.document.write('<head><link rel="stylesheet" href="./styles/styles.css" /></head>');
+    newWin.document.write(modalRoot.outerHTML);
+    newWin.print();
+    newWin.close();
+  };
+
+  const clickOffToCloseModal = (e) => {
+    if (e.target.closest(".modal") === null && e.target.closest(".openModal") === null) {
+      closeModal();
+    }
+  };
+
+  const ESCKeyToCloseModal = (e) => {
+    if (e.key.toLowerCase() === "escape") {
+      closeModal();
+    }
+  };
+
+  const closeModal = () => {
+    modalRoot.classList.remove("show");
+    // remove event listeners (5)
+    modalRoot.querySelector(".closeModal").removeEventListener("click", closeModal);
+    document.removeEventListener("click", clickOffToCloseModal);
+    document.removeEventListener("keydown", ESCKeyToCloseModal);
+    printButton.removeEventListener("click", printRecipe);
+    document.removeEventListener("scroll", updateModalPosition);
+  };
+
+  // Modal Event Listeners (5)
+  // event listener to handle scrolling upwards for modals
+  document.addEventListener("scroll", updateModalPosition);
   // event listener for print button
-  const printButton = modalRoot.querySelector(".printModal");
-  printButton.addEventListener("click", app.printRecipe);
-
+  printButton.addEventListener("click", printRecipe);
   // event listener for the closeModal button
-  const closeModalButton = modalRoot.querySelector(".closeModal");
-  closeModalButton.addEventListener("click", app.closeModal);
+  closeModalButton.addEventListener("click", closeModal);
   // event listener for clicking off the modal
-  document.addEventListener("click", app.clickOffToCloseModal);
+  document.addEventListener("click", clickOffToCloseModal);
   // event listener for ESC Key
-  document.addEventListener("keydown", app.ESCKeyToCloseModal);
+  document.addEventListener("keydown", ESCKeyToCloseModal);
 };
 
-app.clickOffToCloseModal = e => {
-  if (e.target.closest(".modal") === null && e.target.closest(".openModal") === null) {
-    app.closeModal();
-  }
-};
-
-app.ESCKeyToCloseModal = e => {
-  console.log(e);
-  if (e.key.toLowerCase() === "escape") {
-    app.closeModal();
-  }
-};
-
-app.closeModal = () => {
-  const modalRoot = document.querySelector(".modalRoot");
-  modalRoot.classList.remove("show");
-  // remove event listeners (3)
-  modalRoot.querySelector(".closeModal").removeEventListener("click", app.closeModal);
-  document.removeEventListener("click", app.clickOffToCloseModal);
-  document.removeEventListener("keydown", app.ESCKeyToCloseModal);
-};
-
-app.printRecipe = () => {
-  const modalToPrint = document.querySelector(".modalRoot");
-  newWin = window.open("");
-  newWin.document.write('<head><link rel="stylesheet" href="./styles/styles.css" /></head>');
-  newWin.document.write(modalToPrint.outerHTML);
-  newWin.print();
-  newWin.close();
-};
-
-app.getRecipeInfoByID = id => {
+// access API for more detailed recipe info, for use with Modal
+app.getRecipeInfoByID = (id) => {
   const url = new URL(`https://api.spoonacular.com/recipes/${id}/information`);
   url.search = new URLSearchParams({
     apiKey: app.apiKey,
@@ -282,7 +285,7 @@ app.getRecipeInfoByID = id => {
   });
 
   // returns a promise
-  return fetch(url).then(res => res.json());
+  return fetch(url).then((res) => res.json());
 };
 
 // Init method that kicks everything off
@@ -303,9 +306,7 @@ app.init = () => {
   const inputField = document.querySelector("#recipe");
   // used function() format instead of () => format to allow use of "this" to target input field
   inputField.addEventListener("keydown", function (e) {
-    const ingredientLiCount = document
-      .querySelector("#searchContainer ul")
-      .getElementsByTagName("li").length;
+    const ingredientLiCount = document.querySelector("#searchContainer ul").getElementsByTagName("li").length;
     // if enter is pressed and input field is empty and container has li's(ingredients), search for recipes
     if (e.key.toLowerCase() === "enter" && this.value == "" && ingredientLiCount > 0) {
       app.handleSearchForm(e);
